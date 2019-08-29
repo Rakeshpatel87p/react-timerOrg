3fd3d06fcae453791a020b872b41a594752d085e
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-08-29 18:49:39 -0400

    explainatory note

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minutes: 10
+    }
+  }
+  
+  startTimer() {
+    const now = Date.now();
+    const then = now + this.state.minutes * 60 * 1000;
+
+    let countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+      if (secondsLeft < 0) {
+        clearInterval(countdown);
+        return
+      }
+      this.setState({
+        seconds: secondsLeft
+      })
+    }, 1000);
+    
+  }
+
+  componentDidMount() {
+    this.startTimer()
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.minutes}</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
