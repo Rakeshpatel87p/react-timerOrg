0ad411598f1afcfc5525b4cab6e2f055a9f24224
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-09-14 11:48:20 -0400

    new component with state added

--- src/Components/TimerHeader.js
+++ src/Components/TimerHeader.js
@@ -18,9 +18,19 @@ class TimerHeader extends Component {
             <div>
             { this.props.inputToggle ?
                 <form onSubmit={(e) => this.props.handleSubmit(e)}> 
-                    <input onClick={this.props.toggleInput} className="timer timerInput" name="timeInput" placeholder={this.formatTime()} onChange={this.props.handleChange} autoFocus={true} />
+                    <input 
+                        onClick={this.props.toggleInput} 
+                        className="timer timerInput" 
+                        name="timeInput" 
+                        placeholder={this.formatTime()} 
+                        onChange={this.props.handleChange} 
+                        autoFocus={true} 
+                    />
                 </form> : 
-                <h1 className="timer" onClick={this.props.toggleInput}>{this.formatTime()}</h1>
+                <h1 
+                    className="timer" 
+                    onClick={this.props.toggleInput}>{this.formatTime()}
+                </h1>
             }
             </div>
         )
