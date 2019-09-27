02cf938b4e886cf3750efc407bc572b52a3d68cb
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-09-27 07:56:46 +0200

    new component

---
+++ src/Components/UserStats.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserStats extends Component {
+    render() {
+        return (
+            <div>
+                <h1>User Stats</h1>
+            </div>
+        )
+    }
+}
+
+export default UserStats
