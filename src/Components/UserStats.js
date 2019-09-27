a3a69057f591fc18ba1ee6217c8e7b4ac9ac4058
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-09-27 08:30:30 +0200

    skeleton line chart added via chart.js

--- src/Components/UserStats.js
+++ src/Components/UserStats.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import BarChart from './Charts/BarChart';
 
 class UserStats extends Component {
+    constructor(props) {
+        super(props);
+
+        this.myData = [
+            {
+              "name": "A",
+              "value": 46
+            },
+            {
+              "name": "B",
+              "value": 87
+            },
+          ];
+    }
     render() {
+
         return (
             <div>
-                <h1>User Stats</h1>
+                <BarChart 
+                    data={this.myData}
+                    title="Work Over Time"
+                    color="#70CAD1"
+                />
             </div>
         )
     }
