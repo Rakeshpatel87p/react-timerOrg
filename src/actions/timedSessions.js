ad6aed8399593956ab6e78c02951a33c53f6a869
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-10-09 22:41:06 +0200

    talking to firebase now

--- src/actions/timedSessions.js
+++ src/actions/timedSessions.js
@@ -1,10 +1,11 @@
+import { mockSessionsDataRef } from '../firebase';
+
 export const TIMED_SESSIONS = 'TIMED_SESSIONS';
 export const FETCH_TIMED_SESSIONS_BEGIN = 'FETCH_TIMED_SESSIONS_BEGIN';
 export const FETCH_TIMED_SESSIONS_SUCCESS = 'FETCH_TIMED_SESSIONS_SUCCESS';
 export const FETCH_TIMED_SESSIONS_FAILURE = 'FETCH_TIMED_SESSIONS_FAILURE';
 
 export function timedSessions(sessionTime, task = undefined, timestampStart) {
-    console.log({sessionTime, task, timestampStart});
     return {
         type: 'TIMED_SESSIONS',
         sessionTime,
@@ -16,6 +17,11 @@ export function timedSessions(sessionTime, task = undefined, timestampStart) {
 export function fetchTimedSessions() {
     return (dispatch) => {
         dispatch(fetchTimedSessionsBegin())
+        mockSessionsDataRef.once('value').then((snapshot) => {
+            console.log(snapshot.val());
+            dispatch(fetchTimedSessionsSuccess(snapshot.val()));
+        })
+        /*
         fetch('https://jsonplaceholder.typicode.com/todos/1')
             .then(response => response.json())
             .then(res => {
@@ -25,12 +31,13 @@ export function fetchTimedSessions() {
                 setTimeout(() => {
                     dispatch(fetchTimedSessionsSuccess(res));
                 }, 2000)
-                
+
                 return res;
             })
             .catch(error => {
                 dispatch(fetchTimedSessionsFailure(error));
             })
+            */
     }
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOKgsJYYHIemdosMA3v_k5ot_BRy5YCZg",
+    authDomain: "productivity-1f520.firebaseapp.com",
+    databaseURL: "https://productivity-1f520.firebaseio.com",
+    projectId: "productivity-1f520",
+    storageBucket: "productivity-1f520.appspot.com",
+    messagingSenderId: "403436618146",
+    appId: "1:403436618146:web:ea056b2c733abf3af8f2fb",
+    measurementId: "G-VZXRLYM31S"
+};
+
+firebase.initializeApp(firebaseConfig);
+const databaseRef = firebase.database().ref('sessions/');
+console.log(firebase.database().ref('sessions/'));
+
+export const mockSessionsDataRef = databaseRef;
