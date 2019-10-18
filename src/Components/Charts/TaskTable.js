b5966748ffc293eeaf7fb05933f1a83553172544
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-10-18 12:59:40 +0200

    unique key val provided

--- src/Components/Charts/TaskTable.js
+++ src/Components/Charts/TaskTable.js
@@ -7,8 +7,8 @@ class TaskTable extends Component {
             <div>
                 <h2>All Sessions</h2>  
                 <ul>
-                    {this.props.data.map((i, item) => {
-                        return <li>{`task: ${item.task} | time spend: ${item.sessionTime}`}</li>
+                    {this.props.data.map((item, i) => {
+                        return <li key={i}>{`task: ${item.task} | time spend: ${item.sessionTime}`}</li>
                     })}
                 </ul>
             </div>
