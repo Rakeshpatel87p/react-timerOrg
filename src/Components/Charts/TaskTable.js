d64590bf87f685369ab3a361a45e84f8b4a10a7d
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-10-19 17:29:51 +0200

    hide env vars

--- src/Components/Charts/TaskTable.js
+++ src/Components/Charts/TaskTable.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class TaskTable extends Component {
-    state = {  }
     render() { 
         return (
             <div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAOKgsJYYHIemdosMA3v_k5ot_BRy5YCZg",
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
     authDomain: "productivity-1f520.firebaseapp.com",
     databaseURL: "https://productivity-1f520.firebaseio.com",
     projectId: "productivity-1f520",
     storageBucket: "productivity-1f520.appspot.com",
     messagingSenderId: "403436618146",
-    appId: "1:403436618146:web:ea056b2c733abf3af8f2fb",
+    appId: `${process.env.REACT_APP_API_ID}`,
     measurementId: "G-VZXRLYM31S"
 };
 
@@ -15,3 +15,4 @@ firebase.initializeApp(firebaseConfig);
 const databaseRef = firebase.database().ref('mockData/');
 export const rakeshWorkSessions = firebase.database().ref('rakeshWorkSessions/');
 export const mockSessionsDataRef = databaseRef;
+//testing
