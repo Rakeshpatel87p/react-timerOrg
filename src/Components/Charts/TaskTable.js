fbf83a28f5e682b30ca47c4a78ff268dacb76889
rakeshpatel87p <rakesh.patel@accesscfa.com>
2019-10-24 08:27:18 -0400

    final component consuming right data

--- src/Components/Charts/TaskTable.js
+++ src/Components/Charts/TaskTable.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 class TaskTable extends Component {
-    render() { 
+    render() {
+        const { data } = this.props 
         return (
             <div>
                 <h2>All Sessions</h2>  
                 <ul>
-                    {this.props.data.map((item, i) => {
-                        return <li key={i}>{`task: ${item.task} | time spend: ${item.sessionTime}`}</li>
+                    {Object.keys(data).map((item, i) => {
+                        return <li key={i}>{`task: ${data[item].task} | time spend: ${data[item].sessionTime}`}</li>
                     })}
                 </ul>
             </div>
